7500af33393a5ee11bf1b46ccd10371441de70fb
b.lajtar <bartolineon@o2.pl>
2020-10-16 16:56:30 +0200

    Updates

---
+++ precache-manifest.3ed2df545911f053f4ffcb7b06040616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7b4ff90d16f9ee5cd07ea9bf02eec4",
+    "url": "/TicTacToeApp/index.html"
+  },
+  {
+    "revision": "28b3cc99f6dd9114aac8",
+    "url": "/TicTacToeApp/static/css/main.404e248c.chunk.css"
+  },
+  {
+    "revision": "00d2c7ade200d986032f",
+    "url": "/TicTacToeApp/static/js/2.d144e1f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TicTacToeApp/static/js/2.d144e1f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28b3cc99f6dd9114aac8",
+    "url": "/TicTacToeApp/static/js/main.c00bccff.chunk.js"
+  },
+  {
+    "revision": "ce0335b974e87c7bf5df",
+    "url": "/TicTacToeApp/static/js/runtime-main.397f518a.js"
+  }
+]);
